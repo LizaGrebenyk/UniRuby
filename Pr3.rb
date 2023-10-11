13ed83ec54ab2b8cd941e895639d4409ec29ef7e
Lisa <munya01212@gmail.com>
2023-10-11 19:48:47 +0300

    Pr4

--- Pr3.rb
+++ Pr3.rb
@@ -8,9 +8,7 @@ def cut(cake)
   raise "The number of raisins must be greater than 1 and less than 10." unless n > 1 && n < 10
   raise "Cake can`t be cut`" unless s%n==0
 
-  #possible_slices = []
-
-=begin
+=begin      # A simpler option for the case when the cake can be divided into tiers
   (1...cake[0].length).each do |width|
     cake_slices = cake.map { |line| line.scan(/.{#{width}}/) }
 
@@ -23,12 +21,16 @@ def cut(cake)
 =end
 
 
-  possible_slices=split_cake(cake,s0)
+  possible_slices=split_cake(cake,s0)   # Getting all possible pieces with a raisin
 
-  #return possible_slices.max_by { |slices| slices[0][0].length }
+  valid_slices = possible_slices.combination(n).select do |combo|   # Identifying combinations of cut slices, with which can make a primary cake
+    fit_slices(cake.map(&:dup), combo)
+  end
+
+  valid_slices.max_by { |slices| slices[0][0].length }              # Return the combination with the widest first slice
 end
 
-def split_cake(cake,s0)
+def split_cake(cake,s0)     # The method of dividing into rectangles, obtaining all possible slices
   slices = []
 
   heightC = cake.length
@@ -52,7 +54,7 @@ def split_cake(cake,s0)
 end
 
 
-def get_slice(cake, length, width, start_row, start_col)
+def get_slice(cake, length, width, start_row, start_col)    # Obtaining one piece by coordinates and length and width
   slice = []
 
   (start_row...(start_row + length)).each do |i|
@@ -63,20 +65,57 @@ def get_slice(cake, length, width, start_row, start_col)
   slice
 end
 
+def fit_slices(original, slices, fitted = [])   # Placement of cut pieces, backtracking
+  if fitted.size == slices.size                 # If all slices are placed
+    return true
+  end
+
+  slice_to_fit = slices[fitted.size]
+  height = slice_to_fit.size
+  width = slice_to_fit[0].length
+
+  (0..(original.size - height)).each do |i|
+    (0..(original[0].length - width)).each do |j|
+      if can_place?(original, slice_to_fit, i, j)
+        new_original = place_slice(original.map(&:dup), slice_to_fit, i, j)
+
+        return true if fit_slices(new_original, slices, fitted + [slice_to_fit])
+      end
+    end
+  end
+
+  false
+end
+
+def can_place?(cake, slice, row, col)       # If a slice can be placed on the cake
+  slice.each_with_index do |slice_row, i|
+    slice_row.chars.each_with_index do |char, j|
+      if (char == '0' && cake[row + i][col + j] != '0') || (char == '0' && (row + i >= cake.length || col + j >= cake[0].length))
+        # If it's a raisin and it doesn't match the raisin on the cake, or if it've gone beyond the cake
+        return false
+      end
+    end
+  end
+  true
+end
+
+def place_slice(cake, slice, row, col)      # Placement of slice
+  slice.each_with_index do |slice_row, i|
+    slice_row.chars.each_with_index do |char, j|
+      if char == '0'
+        cake[row + i][col + j] = '.'         # Replacing the raisin with a dot to indicate that this part of the cake has already been cut
+      end
+    end
+  end
+  cake
+end
+
 cake=[".0......",
       "......0.",
       "....0...",
       "..0....."]
 
-cake1=[".0.0....",
-       "........",
-      "....0...",
-      "........",
-      ".....0..",
-      "........"]
-
 result = cut(cake)
-#puts result
 result.each do |slice|
   puts slice
   puts "\n"
